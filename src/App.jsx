6bdbb855fb09ab8fab16502f08897026507df057
mateucmc <mateucmc@gmail.com>
2023-12-01 11:56:15 -0300

    novos commits

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,17 @@ function App() {
   return (
     <>
       <Header />
-       conteudo
+       <section className='container'>
+          <div className='apresentacao'>
+            <p>Olá, sou <br/>
+            <span>Edson Maia</span> <br/>
+            Dev full stack</p>
+            <button className='btn'>Sobre mim</button>
+          </div>
+          <figure>
+            <img className='img-home' src="/developer-blue.svg" alt="img" />
+          </figure>
+       </section>
       <Footer />
     </>
   )
